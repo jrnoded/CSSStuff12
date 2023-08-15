74cd90a9e14ab14de6486eff51c8705bd6fdbdc5
Jim Roberts <jrnoded@gmail.com>
2023-08-15 13:49:13 -0400

    Update and Cleanup

--- Photfilter/index.js
+++ Photfilter/index.js
@@ -1,5 +1,5 @@
-
 const filters = [
+    "none",
     "blur",
     "brightness",
     "contrast",
@@ -11,22 +11,20 @@ const filters = [
     "sepia",
     "saturate",
 ];
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+
 const applyFilterButton = document.getElementById("apply-filter");
 const filterImage = document.getElementById("filter-image");
 const msg = document.getElementById("msg");
-let filterOn = false;
+
 applyFilterButton.addEventListener("click", () => {
-    // filterOn = !filterOn;
-    // if (filterOn) {
-        let fltr = filters[getRandomInt(9)] + "(" + getRandomInt(100) + "%)";
-        filterImage.style.filter = fltr;
-        // applyFilterButton.textContent = "Remove Filter";
-    // } else {
-    //     filterImage.style.filter = "none";    
-    //     applyFilterButton.textContent = "Apply Filter";
-    // }
+    let fltr = filters[randomNumber(0, 10)];
+    if (fltr != "none") {
+        fltr += "(" + (randomNumber(25,100)) + "%)";
+    }
+    filterImage.style.filter = fltr;
     msg.innerText = filterImage.style.filter;
 });
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
