d22a79a0c41b03398ef7ac1ddf580db8e11bffdd
Jim Roberts <jrnoded@gmail.com>
2023-09-10 15:21:32 -0400

    Initial Veggie Commit

---
+++ Pagination/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const pages = document.querySelectorAll(".page");
+    const prevButton = document.getElementById("prevPage");
+    const nextButton = document.getElementById("nextPage");
+    let currentPage = 0;
+
+    function showPage(pageNumber) {
+        pages.forEach((page, index) => {
+            if (index === pageNumber) {
+                page.style.display = "block";
+            } else {
+                page.style.display = "none";
+            }
+        });
+    }
+
+    function updateButtons() {
+        prevButton.disabled = currentPage === 0;
+        nextButton.disabled = currentPage === pages.length - 1;
+    }
+
+    prevButton.addEventListener("click", function () {
+        if (currentPage > 0) {
+            currentPage--;
+            showPage(currentPage);
+            updateButtons();
+        }
+    });
+
+    nextButton.addEventListener("click", function () {
+        if (currentPage < pages.length - 1) {
+            currentPage++;
+            showPage(currentPage);
+            updateButtons();
+        }
+    });
+
+    showPage(currentPage);
+    updateButtons();
+});
